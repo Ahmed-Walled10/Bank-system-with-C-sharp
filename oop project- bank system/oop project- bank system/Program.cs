9ac396043f6950065f43eebf6c0599a3f6c659c4
Ahmed Walled <142788591+Ahmed-Walled10@users.noreply.github.com>
2025-01-29 22:25:47 +0200

    Add files via upload

---
+++ oop project- bank system/oop project- bank system/Program.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Specialized;
+namespace oop_project__bank_system
+{
+     public class Start
+    {
+        public Start() 
+        {
+
+            making_account client1 = new making_account();
+            making_account client2 = new making_account();
+            making_account client3 = new making_account();
+            client1._FirstName = "adham";
+            client1._LastName = "saadi";
+            client1._Password = 12341234;
+            client1._Age = 21;
+            client1._Address = "tanta 21 staad";
+            client1._Balance = 0;
+            client1._Id = making_account.s_GlopalId;
+            making_account.s_GlopalId++;
+            making_account.Clients.Add(client1);
+            client2._FirstName = "wael";
+            client2._LastName = "yousri";
+            client2._Password = 12341234;
+            client2._Age = 50;
+            client2._Address = "manzala Main street";
+            client2._Balance = 0;
+            client2._Id = making_account.s_GlopalId;
+            making_account.s_GlopalId++;
+            making_account.Clients.Add(client2);
+            client3._FirstName = "qassem";
+            client3._LastName = "mohamed";
+            client3._Password = 12341234;
+            client3._Age = 21;
+            client3._Address = "el rob3";
+            client3._Balance = 0;
+            client3._Id = making_account.s_GlopalId;
+            making_account.s_GlopalId++;
+            making_account.Clients.Add(client3);
+            while (true)
+            {
+
+
+                Console.WriteLine("                                                  Welcome to our bank");
+                Console.WriteLine("Please select if you are a new or old client");
+                making_account N_A = new making_account();
+                bool HaveAccountflag = false;
+                //sign in or make account
+                while (true)
+                {
+                    Console.WriteLine("1- New client ");
+                    Console.WriteLine("2- Old client");
+                    Console.WriteLine("please enter 1 or 2");
+                    string answer = Console.ReadLine();
+
+                    if (answer == "1")
+                    {
+                        N_A.TakeData();
+                        break;
+                    }
+                    else if (answer == "2")
+                    {
+                        HaveAccountflag = true;
+                        break;
+                    }
+                    else if (!int.TryParse(answer, out int input) || answer != "2" || answer != "1")
+                    {
+                        Console.WriteLine("Invalid input please try again");
+                    }
+
+                }
+
+                Console.WriteLine("Please enter your account number and password to sign in");
+                // signing in
+                string A_N;
+                string A_P;
+                string Cname;
+                while (true)
+                {
+                    while (true)
+                    {
+                        Console.WriteLine("account number(ID):");
+                        A_N = Console.ReadLine();
+                        if (String.IsNullOrEmpty(A_N) || !int.TryParse(A_N, out int input) || A_N.Length != 8)
+                        {
+                            Console.WriteLine("Please enter a valid ID\n");
+                        }
+                        else
+                        { break; }
+                    }
+
+                    while (true)
+                    {
+                        Console.WriteLine("Password:");
+                        A_P = Console.ReadLine();
+                        if (String.IsNullOrEmpty(A_P) || !int.TryParse(A_P, out int input) || A_P.Length != 8)
+                        {
+                            Console.WriteLine("Please enter a valid password from 8 digits\n");
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                    if (N_A.Find_Client(int.Parse(A_N), int.Parse(A_P)) != "-1")
+                    {
+                        Cname = N_A.Find_Client(int.Parse(A_N), int.Parse(A_P));
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("account number or password maybe be wrong please try again");
+                    }
+                }
+
+                //operations
+                while (true)
+                {
+                    Console.WriteLine($"Welcome {Cname}, What do you want to do today");
+                    Console.WriteLine("1- Withdraw\n" +
+                                      "2- Deposit\n" +
+                                      "3-Check the account\n" +
+                                      "4-send money to another account\n" +
+                                      "5-Book an appointment ar the branch\n" +
+                                      "6-Log out");
+                    int ans = int.Parse(Console.ReadLine());
+                    if (ans == 1)
+                    {
+                        N_A.Withdraw();
+                    }
+                    else if (ans == 2)
+                    {
+                        N_A.Deposit();
+                    }
+                    else if (ans == 3)
+                    {
+                        N_A.Check_account();
+                    }
+                    else if (ans == 4)
+                    {
+                        N_A.SendMoney();
+                    }
+                    else if(ans==5)
+                    {
+                        N_A.MakeAppoiment();
+                    }
+                    else if (ans == 6)
+                    {
+                        break;
+                    }
+
+                }
+            }
+
+
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s1=new Start();
+
+        }
+    }
+}
---
+++ oop project- bank system/oop project- bank system/making_account.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_project__bank_system
+{
+
+    public class making_account
+    {
+        public static List<making_account> Clients = new List<making_account>();
+
+        private static int s_DatabaseIndex = 0;
+        public static int s_GlopalId = 10001000;
+        private readonly int _MaxWithdraw = 1000;
+
+        internal string _FirstName;
+        public string Firstname
+        {
+            get { return _FirstName; }
+
+        }
+        internal string _LastName { get; set; }
+        internal int _Password { get; set; }
+        internal int _Age { get; set; }
+        internal string _Address { get; set; }
+        internal int _Id { get; set; }
+        internal decimal _Balance = 0;
+
+
+        making_account the_client;
+        public void TakeData()//(string FirstName, string LastName, int Password, int Age, string Address)
+        {
+            the_client = new making_account();
+            Clients.Add(the_client);
+
+            Console.WriteLine($"Please enter you'r information\n");
+
+            while (true)
+            {
+                Console.WriteLine("First Name:");
+                string FirstName = Console.ReadLine();
+                if (String.IsNullOrEmpty(FirstName) || FirstName.Any(char.IsDigit))
+                {
+                    Console.WriteLine("Please enter a valid name\n");
+                }
+                else
+                {
+                    _FirstName = FirstName;
+                    Clients[s_DatabaseIndex]._FirstName = FirstName;
+                    break;
+                }
+            }//first name
+            while (true)
+            {
+                Console.WriteLine("Last Name:");
+                string LastName = Console.ReadLine();
+                if (String.IsNullOrEmpty(LastName) || LastName.Any(char.IsDigit))
+                {
+                    Console.WriteLine("Please enter a valid name\n");
+                }
+                else
+                {
+                    _LastName = LastName;
+                    Clients[s_DatabaseIndex]._LastName = LastName;
+                    break;
+                }
+            }//last name
+            while (true)
+            {
+                Console.WriteLine("Account Password:");
+                string Password = Console.ReadLine();
+                if (String.IsNullOrEmpty(Password) || !int.TryParse(Password, out int input) || Password.Length > 8 || Password.Length < 8)
+                {
+                    Console.WriteLine("Please enter a valid password from 8 digits\n");
+                }
+                else
+                {
+                    _Password = int.Parse(Password);
+                    Clients[s_DatabaseIndex]._Password = _Password;
+                    break;
+                }
+            }// password
+            while (true)
+            {
+                Console.WriteLine("Age:");
+                string Age = Console.ReadLine();
+                if (String.IsNullOrEmpty(Age) || !int.TryParse(Age, out int input))
+                {
+                    Console.WriteLine("Please enter a valid age\n");
+                }
+                else if (int.Parse(Age) < 14 || int.Parse(Age) > 100)
+                {
+                    Console.WriteLine("the age limit is from 14 to 100");
+                }
+                else
+                {
+                    _Age = int.Parse(Age);
+                    Clients[s_DatabaseIndex]._Age = int.Parse(Age);
+                    break;
+                }
+            }//age
+            while (true)
+            {
+                Console.WriteLine("Address:");
+                string Address = Console.ReadLine();
+                if (String.IsNullOrEmpty(Address))
+                {
+                    Console.WriteLine("Please enter a valid Address\n");
+                }
+                else
+                {
+                    _Address = Address;
+                    Clients[s_DatabaseIndex]._Address = _Address;
+                    break;
+                }
+            }//address
+            _Id = s_GlopalId;
+            Clients[s_DatabaseIndex]._Id = _Id;
+            s_GlopalId++;
+            Console.WriteLine($"You'r account number is {Clients[s_DatabaseIndex]._Id}\n");
+            Clients[s_DatabaseIndex]._Balance = _Balance;
+            s_DatabaseIndex++;
+
+        }
+        making_account The_client;
+        public string Find_Client(int account_ID, int password)
+        {
+            The_client = Clients.Find(c => c._Id == account_ID);
+            if (The_client != null)
+            {
+                if (The_client._Password == password)
+                {
+                    return The_client._FirstName;
+                }
+                else
+                {
+                    return "-1";
+                }
+            }
+            else
+            {
+                return "-1";
+            }
+        }
+        public void Withdraw()
+        {
+
+            Console.WriteLine("How much do you want to withdraw\n");
+            string wanted_money = Console.ReadLine();
+            if (String.IsNullOrEmpty(wanted_money) || !int.TryParse(wanted_money, out int input))
+            {
+                Console.WriteLine("please put valid number");
+            }
+            else if (int.Parse(wanted_money) <= 0 || int.Parse(wanted_money) > _MaxWithdraw)
+            {
+                Console.WriteLine($"sorry but the withdraw is from 0 to {_MaxWithdraw}");
+            }
+            else if (int.Parse(wanted_money) > The_client._Balance)
+            {
+                Console.WriteLine("the account dose not have enought balance\n");
+            }
+            else
+            {
+                The_client._Balance -= int.Parse(wanted_money);
+                Console.WriteLine($"[{DateTime.Now}] successful withdrawing\n" +
+                                  $"you'r current balance is {The_client._Balance}\n");
+            }
+        }
+
+        public void Deposit()
+        {
+            Console.WriteLine("How much do you want to deposit");
+            string amount = Console.ReadLine();
+            if (String.IsNullOrEmpty(amount) || !int.TryParse(amount, out int input) || int.Parse(amount)<=0)
+            {
+                Console.WriteLine("please put valid number");
+            }
+            else
+            {
+                The_client._Balance += int.Parse(amount);
+                Console.WriteLine($"[{DateTime.Now}]the balance has been updated\n" +
+                                  $"you'r current balance is {The_client._Balance}\n");
+            }
+        }
+
+        public void Check_account()
+        {
+            Console.WriteLine($"Name:{The_client._FirstName} {The_client._LastName}\n" +
+                              $"Age:{The_client._Age}\n" +
+                              $"Address:{The_client._Address}\n" +
+                              $"Account number{The_client._Id}\n" +
+                              $"Current balance:{The_client._Balance}\n");
+        }
+
+        public void SendMoney()
+        {
+            Console.WriteLine("please enter account number you want to send money to");
+            string Account_num = Console.ReadLine();
+            if (String.IsNullOrEmpty(Account_num) || !int.TryParse(Account_num, out int input))
+            {
+                Console.WriteLine("please put valid number");
+            }
+            else
+            {
+                making_account transferclient = Clients.Find(c => c._Id == int.Parse(Account_num));
+                if (transferclient == null)
+                {
+                    Console.WriteLine("There is no account at that number please try again");
+                }
+                else
+                {
+                    Console.WriteLine("please enter the amount you want to send");
+                    string theamount = Console.ReadLine();
+                    if (int.Parse(theamount) > The_client._Balance)
+                    {
+                        Console.WriteLine("the account dose not have enough balance\n");
+                    }
+                    else if (int.Parse(theamount) == The_client._Id)
+                    {
+                        Console.WriteLine("you can't send money to yourself");
+                    }
+                    else
+                    {
+                        The_client._Balance -= int.Parse(theamount);
+                        transferclient._Balance += int.Parse(theamount);
+                        Console.WriteLine($"[{DateTime.Now}] successful transfer\n" +
+                                          $"you'r current balance is {The_client._Balance}\n");
+                    }
+                }
+            }
+        }
+
+        private static Dictionary<string, string> weekappoiments = new Dictionary<string, string>();
+        private static bool  AppoimentFlag=false;
+        void theappoiments()
+        {
+            for (int i = 1; i <= 7; i++)
+            {
+                string day = DateTime.Today.AddDays(i).DayOfWeek.ToString();
+                weekappoiments[day]= "empty" ;
+            }
+            AppoimentFlag = true;
+        }
+        
+        public void MakeAppoiment()
+        {
+            if (!AppoimentFlag)
+            {
+                theappoiments();
+            }
+            Console.WriteLine("the avalibale days for appoiments is:");
+            int i = 1;
+            foreach (var entry in weekappoiments)
+            {
+                Console.WriteLine($"{i}-{entry.Key}: {entry.Value}");
+                i++;
+            }
+            Console.WriteLine("\n which day do you want(1-7)");
+            string theday = Console.ReadLine();
+            if (weekappoiments[theday] == "busy")
+            {
+                Console.WriteLine("it's taken please select another day");
+            }
+            else
+            {
+                weekappoiments[theday] = "busy";
+                Console.WriteLine("the appoiment has been reserved \n");
+            }
+        }
+
+    }
+
+
+}
